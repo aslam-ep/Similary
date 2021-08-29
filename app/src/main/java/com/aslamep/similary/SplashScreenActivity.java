47982dc541e140d904f9342cb0b8b746beb21975
mohammed.aslam <mohammed.aslam@qburst.com>
2021-08-29 22:40:22 +0530

    Added splash screen.

---
+++ app/src/main/java/com/aslamep/similary/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.aslamep.similary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //splash scree code
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        },1000);
+    }
+}
